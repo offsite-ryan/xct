621e934751832962ffdb3d4e82733df0cd503b5e
Ryan De Jong <ryan.dejong@avl.com>
2024-07-10 17:13:42 -0400

    ...

--- test-setup.js
+++ test-setup.js
@@ -67,6 +67,8 @@ function closeTest() {
     document.getElementById('test-view-1').style.display = 'none';
     document.getElementById('test-view-2').style.display = 'none';
     document.getElementById('test-view-3').style.display = 'none';
+    document.getElementById('test-view-4').style.display = 'none';
+    document.getElementById('test-view-5').style.display = 'none';
     document.getElementById('test-save-cancel').style.display = 'none';
 }
 
