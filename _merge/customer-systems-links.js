570fc96aac2e819469627cedc19878969068dd67
Ryan De Jong <ryan.dejong@avl.com>
2024-07-23 10:49:47 -0400

    delete merge folder

--- _merge/customer-systems-links.js
+++
@@ -1,5 +0,0 @@
-const systems = {
-    'Development': "http://usplmwn320004:4203/",
-    'Development - Last': "http://usplmwn320004:4203/_last/",
-    'Public': "https://offsite-ryan.github.io/xct/",
-}
