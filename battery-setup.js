e1ffc0207024fc3134adb236ec3e745cc3e06582
Ryan De Jong <ryan.dejong@avl.com>
2024-08-05 11:49:51 -0400

    ...

--- battery-setup.js
+++ battery-setup.js
@@ -5,6 +5,13 @@ function viewBattery(index) {
     // document.getElementById('battery-save-cancel').style.display = 'block';
 }
 
+function viewActiveBattery(index) {
+    const card = cards.items[index];
+    window.location.hash = `configuration-battery-${index}`;
+    document.getElementById('table-batteries-recent').style.display = 'none';
+    document.getElementById(`battery-view-${index}`).style.display = 'block';
+}
+
 function closeBattery() {
     document.getElementById('table-batteries-recent').style.display = 'block';
     document.getElementById('battery-view-1').style.display = 'none';
