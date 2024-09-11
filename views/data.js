eae6cde9e8465b8fd0967f8a0b15c46836c2ba07
Ryan De Jong <ryan.dejong@avl.com>
2024-09-11 10:51:57 -0400

    ...

--- views/data.js
+++ views/data.js
@@ -2,10 +2,10 @@ function loadDataFiles() {
     const obj = { files: [] };
     for (let i = 0; i < 25; i++) {
         let color = i % 7 === 0 ? '#70ad47' : (i % 13 === 0 ? '#fcd00e' : '#c0c0c0');
-        const check = [1, 4, 5, 7, 8].indexOf(i) >= 0 ? '<i class="fa fa-check-square-o color-current"></i>' : '<i class="fa fa-square-o"></i>';
-        const display = [1, 4, 5, 7, 8].indexOf(i) >= 0 ? '' : 'none';
+        const check = '<i class="fa fa-square-o"></i>'; // [1, 4, 5, 7, 8].indexOf(i) >= 0 ? '<i class="fa fa-check-square-o color-current"></i>' : '<i class="fa fa-square-o"></i>';
+        const display = 'none'; // [1, 4, 5, 7, 8].indexOf(i) >= 0 ? '' : 'none';
         const image = `<img src="./assets/data-file-${i === 4 ? 5 : (i === 7 ? 7 : 6)}.png" style="width:350px;height:250px;padding-top:20px;" />`;
-        const image2 = `<img src="./assets/data-file-${i === 4 ? 5 : (i === 7 ? 7 : 6)}.png" style="width:250px;height:250px;padding:20px;" />`;
+        const image2 = `<img src="./assets/data-file-${i === 4 ? 5 : (i === 7 ? 7 : 6)}.png" style="width:300px;height:250px;padding:20px;" />`;
         obj.files.push({
             name: `TXW6323-0${i}_106`,
             project: 'AX345',
@@ -29,14 +29,19 @@ function loadDataFiles() {
     })
 }
 function click_data_file_row(elem) {
+    const index = elem.parentElement.rowIndex - 1;
+    const name = elem.parentElement.children[2].innerText;
+    // console.log(index, name);
     if (elem.children[0].classList.contains('fa-square-o')) {
         elem.children[0].classList.replace('fa-square-o', 'fa-check-square-o');
         elem.children[0].classList.add('color-current');
         elem.parentElement.classList.add('table-row-selected');
+        document.getElementById(`data-file-tile-${name}-2`).style.display = '';
     } else {
         elem.children[0].classList.replace('fa-check-square-o', 'fa-square-o');
         elem.children[0].classList.remove('color-current');
         elem.parentElement.classList.remove('table-row-selected');
+        document.getElementById(`data-file-tile-${name}-2`).style.display = 'none';
     }
 }
 function click_data_file_card(elem) {
